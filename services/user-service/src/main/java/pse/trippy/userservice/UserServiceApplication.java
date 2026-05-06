d6f66e670b3ed6e90109817a712c50c3a6069442
smitunagar <unagarsmit2002@gmail.com>
2026-05-06 15:37:17 +0200

    feat: add logout to profile page, fix JPA/Redis repo conflict, add branding docs

--- services/user-service/src/main/java/pse/trippy/userservice/UserServiceApplication.java
+++ services/user-service/src/main/java/pse/trippy/userservice/UserServiceApplication.java
@@ -2,8 +2,12 @@ package pse.trippy.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "pse.trippy.userservice.repository")
+@EnableRedisRepositories(basePackages = {})
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
