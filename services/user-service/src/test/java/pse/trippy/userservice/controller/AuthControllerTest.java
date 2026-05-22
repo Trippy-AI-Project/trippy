ba53da31017d30805afde24dbf5506baceac536f
Gayathri Gayathri <gayathri.gayathri@mhp.com>
2026-05-23 00:05:26 +0200

    resolve build issues

--- services/user-service/src/test/java/pse/trippy/userservice/controller/AuthControllerTest.java
+++ services/user-service/src/test/java/pse/trippy/userservice/controller/AuthControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import pse.trippy.userservice.config.SecurityConfig;
 import pse.trippy.userservice.dto.request.RegisterRequest;
@@ -32,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @WebMvcTest(AuthController.class)
 @Import(SecurityConfig.class)
+@ActiveProfiles("test")
 @DisplayName("AuthController")
 class AuthControllerTest {
 
